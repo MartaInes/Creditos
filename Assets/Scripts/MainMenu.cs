e12d0ad4bb5ca5cccacfadf54caf02c27b1fe618
MartaInes <73229109+MartaInes@users.noreply.github.com>
2024-10-12 08:36:37 +0200

    create basic scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
